dcff8f14c3cdacc76373c54e1e7b78aff9eb20e2
Ausias <150703040+Ausias03@users.noreply.github.com>
2024-05-21 13:05:29 +0200

    Prototipo de la interfaz para añadir notificaciones personalizadas

---
+++ Codigo/Gestionis/Formularios/ModifNotif.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestionis.Formularios
+{
+    public partial class ModifNotif : Form
+    {
+        public ModifNotif()
+        {
+            InitializeComponent();
+        }
+    }
+}
